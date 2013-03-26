61a2cd1bdfce13f3007705fbee8ed9af9e9b89ed
Nobuyuki Furuyama <furu_nob@yahoo.co.jp>
2013-03-26 17:19:54 +0900

    Add pull_rsync_bw.sh.

---
+++ pull_rsync_bw.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+#RSYNC='/usr/bin/rsync -auvz -e ssh --delete --dry-run'
+#RSYNC='/usr/bin/rsync -auvz --progress -e "ssh" --rsync-path="/usr/bin/sudo /usr/bin/rsync" --delete --dry-run'
+#RSYNC='/usr/bin/rsync -auvz -e ssh --rsync-path="/usr/bin/sudo /usr/bin/rsync $*" --delete --dry-run'
+#RSYNC='/usr/bin/rsync -auvz -e ssh --rsync-path="/home/anaheim/scripts/rsync.sh" --delete --dry-run'
+#RSYNC='/usr/bin/rsync -auvz -e ssh --rsync-path="/home/anaheim/scripts/rsync.sh" '
+#RSYNC='/usr/bin/rsync -auvz --bwlimit=100 -e ssh --rsync-path="/home/anaheim/scripts/rsync.sh" '
+
+REMOTE_USER='anaheim'
+
+# リモートのサーバとそのpath
+REMOTE_SERVER=$1
+REMOTE_PATH=$2
+BANDWIDTH=$4
+
+if [ $BANDWIDTH -eq 0 ]
+then
+  RSYNC='/usr/bin/rsync -auvz -e ssh --rsync-path="/home/anaheim/scripts/rsync.sh" '
+else
+  RSYNC="/usr/bin/rsync -auvz --bwlimit=$4 -e ssh --rsync-path="/home/anaheim/scripts/rsync.sh" "
+fi
+
+ME=`echo $REMOTE_SERVER |sed -e "s/\./_/g"`
+
+# ローカルのバックアップ先
+LOCAL_DESTINATION=$3
+
+# メールの送り先
+mailadd=furuyama@example.net
+# メールの題名
+subject="${ME}_pull_backup_rdiff"
+
+# ログファイル
+log="/var/log/${ME}_rdiff_pull_backup.log"
+
+# Del older file
+older=1M
+
+# Lock File
+_lockfile="/tmp/${ME}_`basename $0`.lock"
+ln -s /dummy $_lockfile 2> /dev/null || { echo "$0 Cannot run multiple instance." | mail -s "Error_$subject" $mailadd && echo "$0 Cannot run multiple instance." >&2; exit 9; }
+trap "rm $_lockfile; exit" 1 2 3 15
+# /Lock File
+
+function backupdir {
+  REMOTEDIR=$1
+  LOCALDIR=$2
+
+  # mktemp
+  temp_file=`mktemp /tmp/temp.XXXXXX`
+
+  echo '---------------8<------------------' >> $temp_logfile
+  echo "start(From $REMOTEDIR To $LOCALDIR) : "`date +%Y-%m-%d-%k:%M.%S` >> $temp_logfile
+
+  ${RSYNC} ${REMOTE_USER}@${REMOTEDIR}/ ${LOCALDIR}/ >> $temp_logfile
+  if [ $? != 0 ]; then
+    echo "It failed in the backup.($REMOTEDIR)" >> $temp_logfile
+    echo "It failed in the backup.($REMOTEDIR)" | mail -s "error_$subject" $mailadd
+  else
+    echo `date +%Y-%m-%d-%k:%M.%S` >> $temp_file
+    echo "From ${ME}:$REMOTEDIR" >> $temp_file
+    echo "To $LOCALDIR" >> $temp_file
+    scp $temp_file $LOCALDIR/../pull_rsync_date.txt
+  fi
+  rm $temp_file
+  echo "finish($REMOTEDIR) : `date +%Y-%m-%d-%k:%M.%S`" >> $temp_logfile
+}
+
+function show_help {
+  echo "****************"
+  echo "$0 usage"
+  echo "****************"
+  echo ""
+  echo "\$ $0 remote_server remote_dir local_dir BANDWIDTH"
+  echo "If BANDWIDTH=0 Unlimit!!"
+  echo ""
+}
+
+if [ $# -ne 4 ]
+then
+  show_help
+  exit 1
+fi
+
+
+temp_logfile=`mktemp /tmp/temp.XXXXXX`
+
+# バックアップ実行
+    #backupdir $REMOTE_USER@$REMOTE_SERVER:$REMOTE_PATH $LOCAL_DESTINATION
+    backupdir $REMOTE_SERVER:$REMOTE_PATH $LOCAL_DESTINATION
+
+cat $temp_logfile >> $log
+
+# メールでログを飛ばす
+cat $temp_logfile | mail -s "$subject" $mailadd
+rm $temp_logfile
+
+# UnLock
+rm $_lockfile
+# /UnLock
+
+
